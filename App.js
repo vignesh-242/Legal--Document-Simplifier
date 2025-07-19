89968173b4967b3b6727cc3baabe564dabb71634
vignesh-242 <vigneshmla24@gmail.com>
2025-07-19 21:54:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LegalDocumentSimplifier from './LegalDocumentSimplifier';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Legal Document Simplifier</h1>
+      <LegalDocumentSimplifier />
+    </div>
+  );
+}
+
+export default App;
